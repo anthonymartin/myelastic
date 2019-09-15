5d2d0b2db881ada0efb881456a94b61e116aab85
Anthony Martin <anthony@replaycreative.com>
2019-09-15 19:49:36 +0200

    updated postinstall.js renamed cli file

--- postinstall.js
+++ postinstall.js
@@ -2,13 +2,31 @@
 const fs = require('fs');
 
 const cwd = process.env.INIT_CWD
+const binDir = cwd + "/bin";
 const envFile = cwd + "/node_modules/@myelastic/indexer/.env.example";
-try {
+const cliFile = cwd + "/node_modules/@myelastic/indexer/lib/cli/myelastic.js";
+
+const copyEnvFile = function () {
   if (fs.existsSync(cwd + "/.env")) {
     // let the user add their own environment variables
   } else {
     fs.copyFileSync(envFile, cwd + "/.env");
-  } 
+  }
+}
+
+const copyCliFile = function () {
+  if (fs.existsSync(binDir)) {
+    fs.copyFileSync(cliFile, binDir + '/myelastic');
+    fs.chmodSync(binDir + '/myelastic', "755");
+  } else {
+    fs.mkdirSync(binDir);
+    fs.copyFileSync(cliFile, binDir + '/myelastic');
+    fs.chmodSync(binDir + '/myelastic', "755");
+  }
+}
+try {
+  copyEnvFile();
+  // copyCliFile();
 } catch (e) {
 
 }
--- src/lib/cli/myelastic.ts
+++ src/lib/cli/myelastic.ts
@@ -1,5 +1,6 @@
-#! /usr/local/bin/ts-node
-import * as yargs from 'yargs'
+#!/usr/bin/env node
+import { Indexer } from '../indexer';
+import * as yargs from 'yargs';
 import lastIndexed from "./cmds/lastIndexed";
 import deleteCmd from "./cmds/delete";
 
